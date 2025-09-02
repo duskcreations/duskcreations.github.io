534069b6f32a44499ce123afd66ba4c2cc58e404
Simon Lane <simon.lane@soton.ac.uk>
2025-09-02 14:46:44 +0100

    removed bats

--- start_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Start a simple local server in this folder and open portfolio.html
-cd /d "%~dp0"
-set PORT=8000
-start "" http://localhost:%PORT%/portfolio.html
-python -m http.server %PORT%
